f86d8048dc4d536809aae6d194000ed1b7857282
Patrick <patrickhua.ty@gmail.com>
2021-01-30 20:28:50 +0800

    all

--- extract.sh
+++ extract.sh
@@ -31,7 +31,8 @@ cd ..
 # Extract the validation data and move images to subfolders:
 #
 mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
-wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+bash valprep.sh
+
 #
 # Check total files after extract
 #
